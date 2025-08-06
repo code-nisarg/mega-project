167e1ea523e864ef9271e4d623d1b1a5a38539d8
Nisarg <nisarganand541@gmail.com>
2025-08-06 23:31:11 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+    <h1>A Blog app for testing with app write</h1>
+    </>
+  )
+}
+
+export default App
